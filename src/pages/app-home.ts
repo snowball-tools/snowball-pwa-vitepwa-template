04e53f916f504772b1f79a4559a66fd25f713850
Vivian Phung <dev+github@vivianphung.com>
2023-10-27 18:01:56 -0400

    vitepwa template start

---
+++ src/pages/app-home.ts
@@ -0,0 +1,88 @@
+import { LitElement, css, html } from 'lit';
+import { property, customElement } from 'lit/decorators.js';
+
+import '@shoelace-style/shoelace/dist/components/card/card.js';
+import '@shoelace-style/shoelace/dist/components/button/button.js';
+
+import { styles } from '../styles/shared-styles';
+
+@customElement('app-home')
+export class AppHome extends LitElement {
+  // For more information on using properties and state in lit
+  // check out this link https://lit.dev/docs/components/properties/
+  @property() message = 'Welcome!';
+
+  static styles = [
+    styles,
+    css`
+      #welcomeBar {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        flex-direction: column;
+      }
+
+      #welcomeCard,
+      #infoCard {
+        padding: 18px;
+        padding-top: 0px;
+      }
+
+      sl-card::part(footer) {
+        display: flex;
+        justify-content: flex-end;
+      }
+
+      @media (min-width: 750px) {
+        sl-card {
+          width: 70vw;
+        }
+      }
+
+      @media (horizontal-viewport-segments: 2) {
+        #welcomeBar {
+          flex-direction: row;
+          align-items: flex-start;
+          justify-content: space-between;
+        }
+
+        #welcomeCard {
+          margin-right: 64px;
+        }
+      }
+    `,
+  ];
+
+  async firstUpdated() {
+    // this method is a lifecycle even in lit
+    // for more info check out the lit docs https://lit.dev/docs/components/lifecycle/
+    console.log('This is your home page');
+  }
+
+  share() {
+    if ((navigator as any).share) {
+      (navigator as any).share({
+        title: 'PWABuilder pwa-starter',
+        text: 'Check out the PWABuilder pwa-starter!',
+        url: 'https://github.com/pwa-builder/pwa-starter',
+      });
+    }
+  }
+
+  render() {
+    return html`
+      <app-header></app-header>
+
+      <main>
+        <div id="welcomeBar">
+          <sl-card id="welcomeCard">
+            <div slot="header">
+              <h2>${this.message}</h2>
+            </div>
+
+            <h1>PWA Template App</h1>
+      </main>
+    `;
+  }
+}
+
